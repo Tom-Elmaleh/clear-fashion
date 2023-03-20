2811b7bfb928d0d55359f824a352919b8b808d44
Tom0103 <elmalehtom@gmail.com>
2023-03-20 17:41:53 +0100

    api done

--- server/api.js
+++ server/api.js
@@ -19,33 +19,10 @@ app.get('/', (request, response) => {
   response.send({'ack': true});
 });
 
-
-// Fetch a specific product given his ID
-async function findProductById(IdProduct) {
-  const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true });
-  const db = client.db(MONGODB_DB_NAME);
-  const collection = db.collection('products');
-  const result = await collection.find({"_id":ObjectId(IdProduct)}).toArray();
-  await client.close();
-  return result;
-}
-
-// Executes a request to get a product
-// http://localhost:8092/products/6410dc4eda67f5f283b46302
-
-
-app.get('/products/:id', async (req, res) => {
-  const IdProduct = req.params.id;
-  const product_id = await findProductById(IdProduct);
-  //res.json(product_id);
-  res.send(product_id);
-});
-
 // Search for specific products
 // limit - number of products to return (default: 12)
 // brand - filter by brand (default: All brands)
 // price - filter by price (default: All price)
- 
 async function SearchProducts(filters,limite) {
   const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true });
   const db = client.db(MONGODB_DB_NAME);
@@ -55,14 +32,11 @@ async function SearchProducts(filters,limite) {
   return result;
 }
 
-// http://localhost:8092/products/search?brand=dedicated&limit=10&price=30
-
-
+// http://localhost:8092/products/search?brand=dedicated&price=39&limit=3
 app.get('/products/search', async (req, res) => {
   const brand = req.query.brand || undefined;
   const limite = req.query.limit || 12;
   const price = req.query.price || undefined;
-  console.log(limite);
   let filters = {}
 
   // filter by brand (default All brands)
@@ -78,6 +52,27 @@ app.get('/products/search', async (req, res) => {
   const products_search = await SearchProducts(filters,parseInt(limite));
   //res.json(products_search);
   res.send(products_search);
+});
+
+
+
+// Fetch a specific product given his ID
+async function findProductById(IdProduct) {
+  const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true });
+  const db = client.db(MONGODB_DB_NAME);
+  const collection = db.collection('products');
+  const result = await collection.find({"_id":ObjectId(IdProduct)}).toArray();
+  await client.close();
+  return result;
+}
+
+// Executes a request to get a product
+// http://localhost:8092/products/6410dc4eda67f5f283b46302
+app.get('/products/:id', async (req, res) => {
+  const IdProduct = req.params.id;
+  const product_id = await findProductById(IdProduct);
+  //res.json(product_id);
+  res.send(product_id);
 });
 
 app.listen(PORT);
